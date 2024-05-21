db927870803bcaad2b42f7a358538944f3bb6b25
Mavi <101255967+mavi-v@users.noreply.github.com>
2024-05-22 00:54:20 +0300

    Add files via upload

---
+++ S.Week2/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace S.Week2
+{
+    public class Point
+    {
+        private int xPos;
+        private int yPos;
+
+        public Point()
+        {
+            xPos = 10;
+            yPos = 20;
+        }
+
+        public Point(int x1, int y1)
+        {
+            xPos = x1;
+            yPos = y1;
+        }
+
+        public void SetPoint(int x, int y)
+        {
+            xPos = x;
+            yPos = y;
+        }
+        public void ShowPoint()
+        {
+            Console.WriteLine("Point: " + xPos + ", " + yPos);
+        }
+    }
+
+    public class Grade
+    {
+        private int studentID;
+        private int studentGrade;
+
+        public Grade(int stID, int stGrade)
+        {
+            if (stID >=1 && stID >= 1000)
+            {
+                studentID = 0;
+            }
+            else
+                studentID = stID;
+
+            if (stGrade > 5 || stGrade < 0)
+            {
+                studentGrade = -1;
+            }
+            else
+                studentGrade = stGrade;
+        }
+
+        //are the "Set" methods needed here also? or is it possible to just request the values from the constructors?
+
+        public int GetStudentID()
+        {
+            return studentID;
+        }
+
+        public int GetGrade()
+        {
+            return studentGrade;
+        }
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+             //Point p1 = new Point();
+             //p1.ShowPoint();
+             const int MAX_SIZE = 4;
+             Point[] points = new Point[MAX_SIZE];
+             for (int i = 0; i < MAX_SIZE; i++)
+             points[i] = new Point();
+             /*points[i] = new Point();
+             points[i] = new Point();
+             points[i] = new Point();*/
+
+             points[0].ShowPoint();
+             points[1].SetPoint(-1, 7);
+             points[1].ShowPoint();
+             points[2].SetPoint(-6, -2);
+             points[2].ShowPoint();
+             points[3].SetPoint(0, 13);
+             points[3].ShowPoint(); 
+
+             //is there anyother way to output and fill this information
+            for ( int index = 0; index < points.Length; index++ )
+             Console.WriteLine("Point {0}:" + points[index].ToString()); //esto esta mal jeje
+            /* Console.WriteLine("Point 1: " points[0].ShowPoint());
+             "Point 2: "  points[1].ShowPoint();*/
+
+            //GRADE CLASS
+            Grade grades = new Grade(123,4);
+            Console.WriteLine("Student ID: " + grades.GetStudentID() + ", Grade: " + grades.GetGrade());
+
+            //while loop
+            //input initialization
+            //try cath structure if the user inputs something iligal to stop the program terminating
+            //info saying the loop is full
+
+
+
+        }
+        
+        /*public class Student
+        {
+            private int studentID;
+            private int credits;
+            private DateTime startDate;
+
+            public Student()
+            {
+                credits = 0;
+                studentID = 130000;
+                startDate = DateTime.Today;
+            }
+        }*/
+
+        
+    }
+}
---
+++ S.week3/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace S.week3
+{
+
+    public class Student
+    {
+        private String studentName;
+        private int studentNr;
+        private int birthYear;
+
+        public Student(string stuName, int stuNr, int year)
+        {
+            studentName = stuName;
+            studentNr = stuNr;
+            birthYear = DateTime.Now.Year - year;
+        }
+
+        public override string ToString()
+        {
+            return studentNr + ": " + studentName + ", age: " + birthYear + " years";
+        }
+    }
+
+    public class Teacher
+    {
+        private String teacherNm;
+
+        public Teacher(string teacName)
+        {
+            teacherNm = teacName;
+        }
+
+        public override string ToString()
+        {
+            return teacherNm;
+        }
+    }
+
+    public class Course
+    {
+        private int credits;
+        private string name;
+        private Teacher teacher_1;
+        private Student student_1;
+
+
+        public Course (string courseName, int creditos) { }
+        public Course(string courseName, int creditos, Teacher nn)
+        {
+            name = courseName;
+            credits = creditos;
+            teacher_1 = nn;
+        }
+
+        public void SetTeacher(string nn)
+        {
+            teacher_1 = new Teacher(nn);
+        }
+
+        /*
+        public void SetStudentsSo(Student list)
+        {
+            students_1 = list;
+        }*/
+
+        //PUT IT ON THE CONSTRUCTOR BECAUSE IT WILL DO IT ANYWAY NO MATTER WHAT HAPPENS
+        private const int MAX_STUDENT = 100;
+        private Student [] students = new Student[MAX_STUDENT];
+        private int nrOfStudents = 0;
+
+        public Course(Student [] stud)
+        {
+            
+            students = stud;
+        }
+
+        public Boolean AddStudent(Student stud)
+        {
+            
+            int i = 0;
+            while (i < MAX_STUDENT && students[i] != null)
+                i++;
+            if (i < MAX_STUDENT)
+            {
+                students[i] = stud;
+                nrOfStudents++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public Student GetStudent(int index)
+        {
+            if (index >= 0 && index < MAX_STUDENT)
+                return students[index];
+            return null;
+        }
+
+        public Student GetStudent(string studNm)
+        {
+            int index = 0;
+            while (index < MAX_STUDENT && !students[index].ToString().Contains(studNm))
+                return students[index];
+            return null;
+        }
+
+        public int GetStudentIndex(string studNm)
+        {
+            int index = 0;
+            while (index < MAX_STUDENT && !students[index].ToString().Contains(studNm))
+                index++;
+            if (index < MAX_STUDENT)
+                return index;
+            return -1;
+        }
+
+        public Student[] GetAllStudents()
+        {
+            Student[] retVal = new Student[nrOfStudents];
+            int index = 0;
+            for (int i = 0; i < MAX_STUDENT; i++)
+            {
+                if (students[i] != null)
+                    retVal[index++] = students[i];
+            }
+            return retVal;
+        }
+
+        public override string ToString()
+        {
+            return "Course " + name + " " + credits + " ECTS; Teacher: " + teacher_1 + ", " + students.ToString() ;
+            //no se como modificar esta parte para que salgan los estudiantes completos
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher t1 = new Teacher("Antti");
+            Course c1 = new Course("Programming", 5, t1);
+            Console.WriteLine(c1.ToString());
+            c1.AddStudent(new Student("Anna",1,1994));
+            c1.AddStudent(new Student("Saara",2,1995));
+            c1.AddStudent(new Student("Jonna", 3, 1996));
+            c1.AddStudent(new Student("Kari", 4, 1995));
+            c1.AddStudent(new Student("Tiina", 5, 1997));
+            foreach (Student stud in c1.GetAllStudents())
+               Console.WriteLine(stud); 
+
+
+
+
+
+
+            //Student myStud = new Student("Anna", 1, 1995);
+
+            //c1.SetTeacher("Saara");
+            //Console.WriteLine(c1.ToString());
+
+
+            //Student myStud = new Student("Kalle", 2, 1995);
+            //Console.WriteLine(myStud);
+
+
+            //if (c1.AddStudent(new Student("John", 1, 1996)))
+            //Console.WriteLine(Student.To
+
+            //Console.WriteLine("Adding new student succesful!");
+            //myStud = c1.GetStudent("John");
+
+            //Console.WriteLine(myStud);
+           /* foreach (Student stud in c1.GetAllStudents())
+                Console.WriteLine(stud);*/
+            //Console.WriteLine(myStud);
+
+
+        }
+
+    }
+}
+
+    
